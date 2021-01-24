d58944dbf0637bae634b79cf485886207ac81046
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-25 01:05:05 +0400

    UseState Hook

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
-const Home = () => {
-    const handleClick = (click) => {
-        console.log("clicked");
-    }
+import { useState } from "react";
 
-    const handleClickMeAgain = (name) => {
-        console.log("Hello " + name);
+const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+
+    const handleClick = () => {
+       setName('luigi');
     }
+    // const handleClickOnce = () => {
+        // setName('pete');
+    // }
+    // const handleClickMeAgain = (name) => {
+        // console.log("Hello " + name);
+    // }
     return ( 
         <div className = 'home'>
-            <h2>Home</h2>
+            <h2>Home</h2> 
+            <p>{name}</p>
             <button onClick= {handleClick}> Click me!</button>
-            <button onClick = {()=> {handleClickMeAgain('Karen')}}>Click me Again!</button>
+            {/* <button onClick= {handleClickOnce}> Click me once more!</button> */}
+            {/* <button onClick = {()=> {handleClickMeAgain('Karen')}}>Click me Again!</button> */}
         </div>
     );
 }
