26056f0d9de4b9e89c8a3a7644fb9dfbee4dfb45
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-25 00:49:21 +0400

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick = (click) => {
+        console.log("clicked");
+    }
+
+    const handleClickMeAgain = (name) => {
+        console.log("Hello " + name);
+    }
     return ( 
         <div className = 'home'>
             <h2>Home</h2>
+            <button onClick= {handleClick}> Click me!</button>
+            <button onClick = {()=> {handleClickMeAgain('Karen')}}>Click me Again!</button>
         </div>
     );
 }
